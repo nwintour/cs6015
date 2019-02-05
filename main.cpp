fb6b3ca95e2bb070aebb69ca0583f508290468a4
natalie <nlwintour@icloud.com>
2019-02-05 06:27:52 -0700

    updates to testing

--- main.cpp
+++ main.cpp
@@ -23,33 +23,38 @@ struct Shape {
     vector<double> slopes;
 };
 
-bool moreThanFiveSpaces(string line) {
-        int spaces = 0; // check spaces for too many points
-        for(char c: line) {
-           // cout << "letter: " << c << endl;
-            if(isspace(c)) {
-                ++spaces;
-              //  cout << "spaces: " << spaces << endl;
-                if(spaces > 5) {
-                    return true;
-                }
-            }
-        }
-    return false;
+vector<string> splitString(string s) {
+    vector<string> result;
+    istringstream iss(s);
+    for(string s; iss >> s; ) {
+        result.push_back(s);
+        // cout << s << endl;
+    }
+    return result;
+}
+
+bool numberPoints(string line) {
+    vector<string> result = splitString(line);
+    if(result.size() != 6) {
+        return false;
+    } else {
+        return true;
+    }
 }
 
 bool containsLetterOrSymbol(string line) {
     for(char c: line) {
-        if(ispunct(c) || !isdigit(c) || isalpha(c)) {
+        if(ispunct(c) || isalpha(c)) {
             return true;
         }
     }
     return false;
 }
 
-bool outOfRange(Shape s) {
-    for(double d: s.points) {
-        if(d > 100 || d < 0) {
+bool outOfRange(vector<string> points) {
+    for(string s: points) {
+        int i = stoi(s);
+        if(i > 100 || i < 0) {
             return true;
         }
     }
@@ -76,45 +81,23 @@ void validateError2(Shape s) { // check for duplicate points
     } // check the other three aren't the same
 }
 
+
 // helper method to validate numbers of points
-string validateError1(string line) {
-
-    if(line.size() == 0 || line.size() > 18 || line.size() < 11) { // check for blank line or line size
+string validateError1(string& line) {
+    if(line.size() == 0) { // check for blank line or line size
         return line = "Error 1: Input size.";
+    } else if(!(numberPoints(line))) {
+        return line = "error 1: spaces";
+    } else if(containsLetterOrSymbol(line)) {
+        return line = "error 1: letter";
     }
-    moreThanFiveSpaces(line);
     
-     char c;
-    for(int i = 0; i < line.size(); ++i) { // check for three numbers in a row or two spaces in a row
-        c = line[i];
-        if(isalpha(c)) {
-            return line = "Error 1: Contains letter.";
-        } else if(!isdigit(c) && !isspace(c)) {
-            return line = "Error 1: Non-alphaneumeric.";
-        } else if(i + 1 > line.size()) {
-            return line;
-        } else if(isdigit(c) && isspace(line[i+1])) {
-            return line;
-        } else if(isspace(c) && isspace(line[i+1])) {
-            return line = "Error 1: Input spacing error.";
-        } else if(i + 2 > line.size()) {
-            return line;
-        } else if(isdigit(c) && isdigit(line[i+1]) && isdigit(line[i+2])) {
-            return line = "Error 1: Input out of bounds.";
-        }
+    vector<string> testingPoints = splitString(line);
+    
+    if(outOfRange(testingPoints)) {
+        return line = "error 1: out of range";
     }
-    return line;
-}
-
-// additional work needed for error 2 exit
-string validateError2(string line) {
-    vector<char> records;
-    for(int i = 0; i < line.size(); ++i) {
-        records.push_back(line[i]);
-    }
-    if(records.size() > 6) { // check again for right number of points
-        return line = "Error 1: Extraneous space or points.";
-    }
+    
     return line;
 }
 
@@ -140,11 +123,8 @@ vector<string> readFile(ifstream& in) {
     string line;
     while(!in.eof()){
         getline(in, line);
-      //  moreThanFiveSpaces(line);
-      //  cout << "true/false S: " << moreThanFiveSpaces(line) << endl;
-      //  cout << "true/false LS: " << containsLetterOrSymbol(line) << endl;
-       // line = validateError1(line);
-       // cout << line << endl;
+        validateError1(line);
+        //cout << line << endl;
         fileLines.push_back(line);
     }
     // vector<string> splitPoints = splitVector(fileLines);
@@ -187,6 +167,17 @@ void stringToDouble(Shape &s, vector<string> positions) {
 
 float slope(float x1, float y1, float x2, float y2) {
     return (y2 - y1) / (x2 - x1);
+}
+
+bool colinear(Shape s) {
+    if(s.slopes.at(0) == s.slopes.at(1)) {
+        return true;
+    } else if(s.slopes.at(1) == s.slopes.at(2)) {
+        return true;
+    } else if(s.slopes.at(2) == s.slopes.at(3)){
+        return true;
+    }
+    return false;
 }
 
 void calculateSlopes(Shape &s) {
@@ -325,22 +316,30 @@ bool isTrap(const Shape &s) {
 
 vector<Shape> makeShapeVector(vector<string> splitBySpace) {
     vector<Shape> allShapes;
+    int marker = 0;
     for(int i = 0; i < splitBySpace.size(); i+= 6) {
-            Shape _shape;
-            vector<string> holdPoints;
-        for(int j = 0; j < 6; ++j) {
-            int marker = i + j;
-            holdPoints.push_back(splitBySpace.at(marker));
-        }
-    
-            stringToDouble(_shape, holdPoints); // points
-   //         cout << "shape points at 0: " << _shape.points.at(4) << endl;
-            calculateSideLength(_shape); // sides
-   //         cout << "shape points at 0: " << _shape.sides.at(3) << endl;
-            calculateSlopes(_shape); // slopes
-   //         cout << "shape points at 0: " << _shape.slopes.at(2) << endl;
-            
+        Shape _shape;
+        if(isalpha(splitBySpace.at(i)[0])) {
+            _shape.error = splitBySpace.at(i);
+            i -= 5;
             allShapes.push_back(_shape);
+        } else {
+            vector<string> holdPoints;
+            for(int j = 0; j < 6; ++j) {
+                marker = i + j;
+                holdPoints.push_back(splitBySpace.at(marker));
+            }
+            stringToDouble(_shape, holdPoints); // points
+            //         cout << "shape points at 0: " << _shape.points.at(4) << endl;
+            calculateSideLength(_shape); // sides
+            //         cout << "shape points at 0: " << _shape.sides.at(3) << endl;
+            calculateSlopes(_shape); // slopes
+            //         cout << "shape points at 0: " << _shape.slopes.at(2) << endl;
+            if(colinear(_shape)) {
+                _shape.error = "error 3";
+            }
+            allShapes.push_back(_shape);
+        }
     }
     return allShapes;
 }
@@ -350,7 +349,7 @@ void printShapes(vector<Shape> &all) {
     for(Shape s: all) {
         if(s.error != "") {
             cout << s.error << endl;
-            EXIT_FAILURE;
+           // EXIT_FAILURE;
         } else {
             if (isSquare(s) == true) {
                 cout << "square" << endl;
@@ -378,24 +377,33 @@ int main(int argc, const char * argv[]) {
     
 
     // this is tested
-    ifstream in ( argv[1] );
+    ifstream in (argv[1]);
     ofstream out(argv[2]);
-    cout.rdbuf(out.rdbuf()); //redirect std::cout to out.txt!
-    
-//    string inTxt = argv[1];
-//    cout << inTxt << endl;
-//    string outTxt = argv[2];
-//    cout << outTxt << endl;
+    cout.rdbuf(out.rdbuf());
 
-  //  ifstream in("testBasic.txt");
+  //  ifstream in ("testBasic.txt");
+  //  ifstream in ("testgen.txt");
+
+//      ifstream in ("randomSquares.txt");
   // ifstream in("randomRhombi.txt");
-  //  ifstream in("randomKites.txt");
- //   ifstream in("randomQuadrilaterals.txt");
+ //  ifstream in("randomKites.txt");
+  // ifstream in("randomQuadrilaterals.txt");
 //    ifstream in("randomTrapezoids.txt");
 //    ifstream in("randomParallelograms.txt");
-//
+
+//    string test = "16 33 49 49 33 16";
+//    cout <<  numberPoints(test) << endl;
+//    vector<string> test2 = splitString(test);
+//    for(string c: test2) {
+//        cout << c << endl;
+//    }
+//    cout << outOfRange(test2);
+    
     vector<string> inFile = readFile(in);
     vector<string> splitBySpace = splitVector(inFile);
+   // cout << "size before split: " << inFile.size() << endl;
+  //  printVector(splitBySpace);
+  //  cout << "size after split: " << splitBySpace.size() << endl;
     vector<Shape> allShapes = makeShapeVector(splitBySpace);
     printShapes(allShapes);
 //    out.close();
--- makefile
+++ makefile
@@ -6,17 +6,11 @@ assignment3: main.cpp
 make: all
 	clang++ -std=c++14 main.cpp -o main
 
-configure: all
+configure:
 	./configure
 
 test: all
 	./test.sh
-
-coverage:
-	clang++ -fprofile-instr-generate -fcoverage-mapping main.cpp -o main
-	LLVM_PROFILE_FILE="main.profraw" ./main
-	xcrun llvm-profdata merge -sparse main.profraw -o main.profdata
-	xcrun llvm-cov show ./main -instr-profile=main.profdata
 
 clean:
 	rm -f main
